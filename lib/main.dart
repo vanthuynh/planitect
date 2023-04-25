1a6dcd77f43ab8104906d0ef29ecc6cd90895df1
vanthuynh <vthuynh@cpp.edu>
2023-04-25 16:29:14 -0700

    add dependencies for firebase_auth

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:van_main_flutter_app/views/onboarding_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 
 void main() async {
