4b2311d7ebc68b02f3d29052bb8240aaf81a7479
vanthuynh <vthuynh@cpp.edu>
2023-04-25 10:35:01 -0700

    first onboarding screen

---
+++ lib/views/onboarding_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class OnBoardingScreen extends StatelessWidget {
+  // const OnBoardingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("Welcome to EMS!",style: TextStyle(
+            color: Colors.black,
+            fontSize: 27,
+            fontWeight: FontWeight.w700,
+
+          ),)
+        ],
+      ),
+    );
+  }
+}
