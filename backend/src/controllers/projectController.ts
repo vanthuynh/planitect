cafaa2ea3d7fd500a761d40176e3457f0fe42ff4
vanthuynh <vthuynh@cpp.edu>
2025-06-07 18:46:16 -0700

    add POST request to create a project

--- backend/src/controllers/projectController.ts
+++ backend/src/controllers/projectController.ts
@@ -9,8 +9,32 @@ export const getProjects = async (
 ): Promise<void> => {
   try {
     const projects = await prisma.project.findMany();
-    res.json(projects);
-  } catch (error) {
-    res.status(500).json({ message: "Error retrieving projects * " });
+    res.status(201).json(projects);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error retrieving projects: ${error.message}` });
+  }
+};
+
+export const createProject = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { name, description, startDate, endDate } = req.body;
+  try {
+    const newProject = await prisma.project.create({
+      data: {
+        name,
+        description,
+        startDate,
+        endDate,
+      },
+    });
+    res.status(201).json(newProject);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error creating project: ${error.message}` });
   }
 };
--- backend/src/routes/projectRoutes.ts
+++ backend/src/routes/projectRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getProjects } from "../controllers/projectController";
+import { getProjects, createProject } from "../controllers/projectController";
 
 const router = Router();
 
 router.get("/", getProjects); // use route list from separate files, and make GET request using getProjects logic
+router.post("/", createProject);
 
 export default router;
