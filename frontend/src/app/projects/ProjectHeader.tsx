63827de5a98585c373a123d9aaaf4075533c788a
vanthuynh <vthuynh@cpp.edu>
2025-06-08 00:40:48 -0700

    add the rest of Tabs and search,filter button

--- frontend/src/app/projects/ProjectHeader.tsx
+++ frontend/src/app/projects/ProjectHeader.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/Header";
-import { Grid3X3 } from "lucide-react";
+import { Clock, Filter, Grid3X3, List, Share2, Table } from "lucide-react";
 import React, { useState } from "react";
 
 type Props = {
@@ -26,6 +26,40 @@ const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
             setActiveTab={setActiveTab}
             activeTab={activeTab}
           />
+          <TabButton
+            name="List"
+            icon={<List className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="Timeline"
+            icon={<Clock className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="Table"
+            icon={<Table className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+        </div>
+        <div className="flex items-center gap-2">
+          <button className="text-gray-500 hover:text-gray-600 dark:text-neutral-500 dark:hover:text-gray-300">
+            <Filter className="h-5 w-5" />
+          </button>
+          <button className="text-gray-500 hover:text-gray-600 dark:text-neutral-500 dark:hover:text-gray-300">
+            <Share2 className="h-5 w-5" />
+          </button>
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Search Task"
+              className="rounded-md border py-1 pl-10 pr-4 focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white"
+            />
+            <Grid3X3 className="absolute left-3 top-2 h-4 w-4 text-gray-400 dark:text-neutral-500" />
+          </div>
         </div>
       </div>
     </div>
